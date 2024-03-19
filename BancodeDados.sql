b63d491ab5482a2e4b1ee1bdfe5e2680edaeee88
Serpulus <135248305+SkiaLupus@users.noreply.github.com>
2024-03-18 22:14:37 -0400

    Update BancodeDados.sql

--- BancodeDados.sql
+++ BancodeDados.sql
@@ -1,7 +1,9 @@
 CREATE TABLE usuarios (
     id  int primary key not null auto_increment,
     nome varchar(50) not null unique,
-    email varchar(100) not null unique,
+    email varchar(100) not null unique
+    telefone varchar(11) not null unique,
+    sexo varchar(15) not null,
     data_nascimento date not null,
     senha varchar(10) not null unique,
     estado VARCHAR(100) not null,
